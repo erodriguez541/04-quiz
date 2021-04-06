c166ac83836657ed89ca7585a80d1029cd41fe79
ErnieRodriguez <erod541@gmail.com>
2021-04-06 12:15:45 -0500

    Adding a question array

---
+++ assets/script.js
@@ -0,0 +1,50 @@
+var countdown = document.querySelector("countdown");
+
+//make a question array
+var questions = [
+  {
+    question: "What does the DOM stand for?",
+    answerone: "Do One More",
+    answertwo: "Document Object Model",
+    answerthree: "Device Object Model",
+    correctanswer: "Document Object Model"
+  }
+]
+///timer
+document.getElementById(countdown).addEventListener("click", countdown)
+countdown();{
+  //  setup a timer
+  const intervalId = setInterval(() => {
+      // update the timer  
+      this.timeRemaining -= 1000;
+
+      if (this.timeRemaining < 0) {
+          // call the callback
+          complete();
+
+          // clear the interval if expired
+          clearInterval(intervalId);
+      }
+
+  }, 1000);
+}
+
+
+
+
+//let timerInterval = null;
+
+//document.getElementById("countdown").innerHTML = `...`
+
+//function startTimer() {
+//  timerInterval = setInterval(() => {
+    
+    // The amount of time passed increments by one
+  //  timePassed = timePassed += 1;
+    //timeLeft = TIME_LIMIT - timePassed;
+    
+    // The time left label is updated
+    //document.getElementById("base-timer-label").innerHTML = formatTime(timeLeft);
+//  }, 1000);
+//}
+
