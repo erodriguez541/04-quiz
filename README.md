0b83a4a29f3fa9688f620532d3f1a6e98d99a99c
ErnieRodriguez <erod541@gmail.com>
2021-04-07 22:16:31 -0500

    stuck on timer calling it a night

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 04-quiz
 Quiz Game
+
+# Description
+Creating a game with Javascript that keeps score for a user while also counting down the timer.  
--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,44 @@
-var countdown = document.querySelector("countdown");
-var countdown = setInterval(60)
+var questioncontainer = document.querySelector("#question-container");
+var timer = document.querySelector(".countdown");
+
+
 
 //make a question array
 var questions = [
   {
     question: "What does the DOM stand for?",
-    answerone: "Do One More",
-    answertwo: "Document Object Model",
-    answerthree: "Device Object Model",
-    correctanswer: "Document Object Model"
+    options: ["Do One More", "Document Object Model", "Device Object Model","Document Object Model"],
+    answer: "Document Object Model"
+  },
+  { 
+  question: "What's your name?",
+    options: ["Name", "Ernie","Test"],
+    answer: "Ernie"
   }
 ]
 console.log(questions)
 ///timer
-var timer = setInterval(0)
-document.getElementsByClassName(countdown).addEventListener("click", countdown);{
-  //  setup a timer
-  const intervalId = setInterval(() => {
-      // update the timer  
-      this.timeRemaining -= 1000;
+var secondsLeft= 60;
 
-      if (this.timeRemaining < 0) {
-          // call the callback
-          complete();
 
-          // clear the interval if expired
-          clearInterval(intervalId);
-      }
-
+timer.addEventListener("click", startTimer(){
+  var timer = setInterval(function() {
+    console.log(count);
+    count--;
+    if(count === 0) {
+      stopInterval()
+    }
   }, 1000);
-}
+  
+  var stopInterval = function() {
+    console.log('time is up!');
+    clearInterval(timer);
+  }
 
-
+//Create a function to pull in the next question
+//Create a score
 
 
 //let timerInterval = null;
 
-//document.getElementById("countdown").innerHTML = `...`
-
-//function startTimer() {
-//  timerInterval = setInterval(() => {
-    
-    // The amount of time passed increments by one
-  //  timePassed = timePassed += 1;
-    //timeLeft = TIME_LIMIT - timePassed;
-    
-    // The time left label is updated
-    //document.getElementById("base-timer-label").innerHTML = formatTime(timeLeft);
-//  }, 1000);
-//}
 
